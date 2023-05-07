7495fe13b2c51f4fcedf0e675dc1fb9272c520fe
fruspa <25087275+fruspa@users.noreply.github.com>
2023-05-07 14:15:24 +0200

    Add compute phase to node application
    Added the computational phase to the main node application (./apps/node).
    Currently, the app executes a test circuit (identity + CKS) with fixed
    aggregator and receivers. In particular, the aggregator has ID "cloud",
    and the only party providing input has ID "node-a", this party is also
    the only receiver. In the future, it will be possible to specify the details
    of the computational phase using a compute descriptor.
    
    To maintain backward compatibility and leave unchanged the current
    scenarios, it is possible to specify whether the node should execute the
    compute phase with the flag -docompute (disabled by default).

--- apps/node/main.go
+++ apps/node/main.go
@@ -7,13 +7,18 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 
+	"github.com/ldsec/helium/pkg/protocols"
 	pkg "github.com/ldsec/helium/pkg/session"
 	"github.com/ldsec/helium/pkg/utils"
+	"github.com/tuneinsight/lattigo/v4/bfv"
+	"github.com/tuneinsight/lattigo/v4/rlwe"
 
 	"github.com/ldsec/helium/pkg/node"
+	"github.com/ldsec/helium/pkg/services/compute"
 	"github.com/ldsec/helium/pkg/services/setup"
 )
 
@@ -27,6 +32,8 @@ var (
 	insecureChannels = flag.Bool("insecureChannels", false, "run the MPC over unauthenticated channels")
 	tlsdir           = flag.String("tlsdir", "", "a directory with the required TLS cryptographic material")
 	outputMetrics    = flag.Bool("outputMetrics", false, "outputs metrics to a file")
+	docompute        = flag.Bool("docompute", false, "whether to execute the compute phase")
+	computeFile      = flag.String("compute", "/helium/config/compute.json", "the compute description file")
 )
 
 // Instructions to run: go run main.go node.go -config [nodeconfigfile].
@@ -95,19 +102,166 @@ func main() {
 		panic(errConn)
 	}
 
+	setupPhase(node, nc, sd, nl)
+
+	if *docompute {
+		computePhase(node, nc, sd, nl)
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+	log.Printf("%s | exiting.", nc.ID)
+}
+
+func setupPhase(node *node.Node, nc node.Config, sd setup.Description, nl pkg.NodesList) {
 	start := time.Now()
-	err = node.GetSetupService().Execute(sd, nl)
+	err := node.GetSetupService().Execute(sd, nl)
 	if err != nil {
-		log.Printf("Node %s | execute returned an error: %s", nc.ID, err)
+		log.Printf("Node %s | SetupService.Execute() returned an error: %s", nc.ID, err)
 	}
 	elapsed := time.Since(start)
+	outputStats(node, nc, sd, nl, elapsed)
+}
+
+func computePhase(node *node.Node, nc node.Config, sd setup.Description, nl pkg.NodesList) {
+	// register all circuits in the global state of the compute service
+	registerCircuits()
+
+	cloudID := nl[0].NodeID
+	var cSign = compute.Signature{
+		CircuitName: "Identity",
+		Delegate:    cloudID,
+	}
+
+	cLabel := pkg.CircuitID("test-circuit-0")
+	sessionID := pkg.SessionID(nc.SessionParameters[0].ID)
+	sess, exists := node.GetSessionFromID(sessionID)
+	if !exists {
+		panic(fmt.Errorf("No session found for ID: %s", sessionID))
+	}
+	ctx := pkg.NewContext(&sessionID, nil)
+
+	computeService := node.GetComputeService()
+	err := computeService.LoadCircuit(ctx, cSign, cLabel)
+	if err != nil {
+		panic(err)
+	}
+
+	// set cryptographic environment
+	// rlweParams, err := rlwe.NewParametersFromLiteral(nc.SessionParameters[0].RLWEParams)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	bfvParams, err := bfv.NewParameters(*sess.Params, 65537)
+	if err != nil {
+		panic(err)
+	}
+	kg := rlwe.NewKeyGenerator(*sess.Params)
+	_, recPk := kg.GenKeyPair()
+	sess.RegisterPkForNode("node-a", *recPk)
+
+	encoder := bfv.NewEncoder(bfvParams)
+
+	var outCtList []pkg.Operand
+	// client executes
+	if node.ID() == "node-a" {
+		cpk := new(rlwe.PublicKey)
+		err = sess.ObjectStore.Load(protocols.Signature{Type: protocols.CKG}.String(), cpk)
+		if err != nil {
+			panic(fmt.Errorf("%s | CPK was not found for node %s: %s", sess.NodeID, sess.NodeID, err))
+		}
+		encryptor := bfv.NewEncryptor(bfvParams, cpk)
+
+		// craft input
+		inData := []uint64{42}
+		inPt := encoder.EncodeNew(inData, bfvParams.MaxLevel())
+		inCt := encryptor.EncryptNew(inPt)
+		// "//nodeID//circuitID/inputID"
+		opLabel := pkg.OperandLabel(fmt.Sprintf("//%s/%s/in-0", node.ID(), cLabel))
+		op := pkg.Operand{OperandLabel: opLabel, Ciphertext: inCt}
+
+		// execute
+		start := time.Now()
+		outCtList, err = computeService.Execute(ctx, cLabel, op)
+		if err != nil {
+			panic(fmt.Errorf("[Compute] Client ComputeService.Execute() returned an error: %s", err))
+		}
+		elapsed := time.Since(start)
+		outputStats(node, nc, sd, nl, elapsed)
+
+	} else {
+		// cloud and node-b execute
+		start := time.Now()
+		outCtList, err = computeService.Execute(ctx, cLabel)
+		if err != nil {
+			panic(fmt.Errorf("[Compute] Cloud ComputeService.Execute() returned an error: %s", err))
+		}
+		elapsed := time.Since(start)
+		outputStats(node, nc, sd, nl, elapsed)
+	}
+
+	// retrieve output
+	if len(outCtList) <= 0 {
+		log.Println("No outputs to retrieve")
+		return
+	}
+	outCt := outCtList[0]
+	if outCt.Ciphertext == nil {
+		panic(fmt.Errorf("Output ciphertext is nil"))
+	}
+	log.Printf("[Compute] Got encrypted output: %v", outCt)
+	if sess.Sk == nil {
+		log.Printf("Refusing to decrypt output: the session secret key is nil. Is this node supposed to have it?\n")
+		return
+	}
+	decryptor := bfv.NewDecryptor(bfvParams, sess.Sk)
+	outPt := encoder.DecodeUintNew(decryptor.DecryptNew(outCt.Ciphertext))
+
+	log.Printf("[Compute] Retrieved output: %v\n", outPt)
+}
+
+func registerCircuits() {
+	testCircuits := map[string]compute.Circuit{
+		"Identity": func(ec compute.EvaluationContext) error {
+			// input from node-a
+			opIn := ec.Input("//node-a/in-0")
+
+			// output encrypted under CPK
+			opRes := pkg.Operand{OperandLabel: "//cloud/out-0", Ciphertext: opIn.Ciphertext}
+
+			// collective key switching
+			params := ec.Parameters().Parameters
+			opOut, err := ec.CKS("DEC-0", opRes, map[string]string{
+				"target":     "node-a",
+				"aggregator": "cloud",
+				"lvl":        strconv.Itoa(params.MaxLevel()),
+				"smudging":   "1.0",
+			})
+			if err != nil {
+				return err
+			}
+
+			// output encrypted under node-a public key
+			ec.Output(opOut, "node-a")
+			return nil
+		},
+	}
+	for label, cDef := range testCircuits {
+		if err := compute.RegisterCircuit(label, cDef); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func outputStats(node *node.Node, nc node.Config, sd setup.Description, nl pkg.NodesList, elapsed time.Duration) {
 	log.Printf("%s | finished setup for N=%d T=%d", nc.ID, len(nl), nc.SessionParameters[0].T)
 	log.Printf("%s | execute returned after %s", nc.ID, elapsed)
 	log.Printf("%s | network stats: %s", nc.ID, node.GetTransport().GetNetworkStats())
 
 	if *outputMetrics {
 		var statsJSON []byte
-		statsJSON, err = json.MarshalIndent(map[string]string{
+		statsJSON, err := json.MarshalIndent(map[string]string{
 			"N":        fmt.Sprint(len(nl)),
 			"T":        fmt.Sprint(nc.SessionParameters[0].T),
 			"Wall":     fmt.Sprint(elapsed),
@@ -120,9 +274,4 @@ func main() {
 			log.Println(errWrite)
 		}
 	}
-
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	<-sigs
-	log.Printf("%s | exiting.", nc.ID)
 }
